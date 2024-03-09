c37ff1c70412f29d8dd2ff1ac8fb2f26ec265888
Venkata Sai Sreyas Adury <venkata.adury0307@gmail.com>
2024-03-09 12:23:05 -0500

    Added a scripts folder and a script to get TMaMD output

---
+++ examples/scripts/get_TMaMD_files.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This script will get the necessary file to run TMaMD_PostProcess notebook from examples
+
+# --- User-specific details (Start) --- #
+USERNAME="vadury"
+SERVER="bridges2.psc.edu"
+DATA_SERVER="data.bridges2.psc.edu"
+GET_PATH="/jet/home/vadury/r1128/thermomaps/example4"
+# --- User-specific details (End) --- #
+
+ssh -t $USERNAME@$SERVER "cd $GET_PATH/data; zip -rT ship.zip iter*/mount/xtc rosetta"
+scp $USERNAME@$DATA_SERVER:$GET_PATH/data/ship.zip .
+unzip ship.zip
+if test $? -eq 0
+then
+	rm ship.zip
+fi
+echo "Done"
