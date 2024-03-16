592915362c81624498488611054bc53185c3ffe5
Venkata Sai Sreyas Adury <venkata.adury0307@gmail.com>
2024-03-16 16:36:30 -0400

    get_TMaMD_files now also gets the multitraj. python/General is now improved with a GriddedData object that can compute interpolated values on a 2D grid

--- examples/scripts/get_TMaMD_files.sh
+++ examples/scripts/get_TMaMD_files.sh
@@ -9,7 +9,7 @@ DATA_SERVER="data.bridges2.psc.edu"
 GET_PATH="/jet/home/vadury/r1128/thermomaps/example4"
 # --- User-specific details (End) --- #
 
-ssh -t $USERNAME@$SERVER "cd $GET_PATH/data; zip -rT ship.zip iter*/mount/xtc rosetta iter*/multitraj.h5"
+ssh -t $USERNAME@$SERVER "cd $GET_PATH/data; zip -rT ship.zip iter*/mount/xtc rosetta iter*/mount/multitraj.h5 iter*/mount/figs"
 scp $USERNAME@$DATA_SERVER:$GET_PATH/data/ship.zip .
 unzip ship.zip
 if test $? -eq 0
